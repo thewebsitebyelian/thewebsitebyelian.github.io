5205ed66806e96563adb1cdeb0d1610195e30c3e
thewebsitebyelian <evasque4330@student.gwinnetttech.edu>
2024-11-17 16:23:01 -0500

    Add files via upload

---
+++ chapter10/lab01/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+Elian Vasquez
+File: script.js
+Date: 11/3/24 
+*/
+
+/* Hamburger menu function. */
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+    menu.style.display = "none";
+    } else {
+    menu.style.display = "block";
+    }
+    }

fd13b28c50bb9eff6925d2f77f9d07262938f580
thewebsitebyelian <evasque4330@student.gwinnetttech.edu>
2024-11-19 22:43:50 -0500

    Add files via upload

---
+++ chapters/chapter12/lab02/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Elian Vasquez
+    File Name: script.js
+    Date: 11/17/24
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+    });
